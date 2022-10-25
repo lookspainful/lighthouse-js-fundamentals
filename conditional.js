d66b3e48a3f1ec25c0e59ac0e0048fd62d2d3bcd
Spencer Chisholm <spencer.chisholm@live.ca>
2022-10-25 21:05:41 +0000

    Add more conditional examples

--- conditional.js
+++ conditional.js
@@ -4,8 +4,15 @@ const temperature = 12;
 if (raining) {
   console.log("Don't forget your umbrella!");
 }
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
 
-if (temperature < 0) {
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+else if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
   console.log("Short sleeves won't cut it!");
